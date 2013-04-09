7df9fa4a61739078d0b85f7840e2dae37c1d8b0d
App Academy Student <student@appacademy.io>
2013-04-09 09:17:43 -0700

    ze first commit, jah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackernewsclone::Application.config.secret_token = 'f178e38759f4faae8f558a24a2b4d2724fc89562d27e192b06033b4b81b07b15aa192c1ce39205735c857f848e893c5c30e91151ec930c9df567713549543b81'
